4c18a893145d17fd27d2101b431656a1e3c35786
Prav <pravir.raghu@hotmail.com>
2022-03-16 20:24:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddSwaggerGen(options =>
     options.SwaggerDoc("v1", new OpenApiInfo
     {
         Version = "v1",
-        Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
+        Title = "NetMicroserviceTemplate",
+        Description = "An ASP.NET 6 Web API / Microservice Template",
         TermsOfService = new Uri("https://example.com/terms"),
         Contact = new OpenApiContact
         {
