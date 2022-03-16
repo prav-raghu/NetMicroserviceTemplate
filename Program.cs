3493fcb37bcf2f270a608391afd6c0a97c1e2a62
Prav <pravir.raghu@hotmail.com>
2022-03-16 20:15:09 +0200

    added some changes to the swagger docs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
 builder.Services.AddDbContext<NetMicroserviceTemplate.Data.UserDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("UserDbConnection"));
@@ -14,9 +34,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     // [1]
-    app.UseSwaggerUI(c => {
-     c.SwaggerEndpoint("/swagger/v1/swagger.json", "NetMicroserviceTemplate");
-     c.RoutePrefix = "documentation";
+    
+    app.UseSwaggerUI(options => {
+     options.SwaggerEndpoint("/swagger/v1/swagger.json", "NetMicroserviceTemplate");
+     options.RoutePrefix = string.Empty;
  });
 }
 app.UseAuthorization();
